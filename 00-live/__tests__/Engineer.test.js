1d42ebd9767e72666bbfc542161055f7d7519c75
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-07-29 22:24:36 -0500

    first employee test works

--- 00-live/__tests__/Engineer.test.js
+++ 00-live/__tests__/Engineer.test.js
@@ -1,9 +1,9 @@
 const Engineer = require('../lib/Engineer');
 
 describe('Engineer class', () => {
-    it('should extend Employee class', () => {
+    it('should extend the Employee class ', () => {
         const engineer = new Engineer();
         expect(typeof (engineer)).toBe('object');
-        expect(employee instanceof Engineer).toBe(true);
+        expect(engineer instanceof Engineer).toBe(true);
     })
 })
--- 00-live/lib/Engineer.js
+++ 00-live/lib/Engineer.js
@@ -1,2 +1,14 @@
-const employee = require('./Employee');
+
 const inquirer = require('inquirer');
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+    constructor(id, name, email, github) {
+    super(id, name, email);
+    this.github = github;
+    super.role = "Engineer";
+    }
+
+}
+
+module.exports = Engineer;

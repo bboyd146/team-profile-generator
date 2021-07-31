b97099e6a87a7db500b190a028d60dad5e99f119
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-07-30 20:45:20 -0500

    Intern class set up

--- 00-live/lib/Intern.js
+++ 00-live/lib/Intern.js
@@ -1,2 +1,19 @@
-const employee = require('./Employee');
+const Employee = require('./Employee');
 const inquirer = require('inquirer');
+class Intern extends Employee {
+    constructor(id, name, email, school) {
+    super(id, name, email);
+    this.school = school;
+    super.role = "Intern";
+    }
+
+    getSchool() {
+        return this.school;
+    }
+
+    getRole() {
+        return this.role;
+    }
+}
+
+module.exports = Intern;

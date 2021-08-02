3267888c70fd61a0429779aeac1f63abec409c56
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-01 20:19:02 -0500

    changed const to function for prompt

---
+++ 00-live/index.js
@@ -0,0 +1,84 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+const employees = [];
+
+// TODO: Create an array of questions for user input
+function newEmployee() { 
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter team member's name",
+            name: "name"
+        },
+        {
+            type: "list",
+            message: "Select team member's role",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager"
+            ],
+            name: "role"
+        },
+        {
+            type: "input",
+            message: "Enter team member's id",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter team member's email address",
+            name: "email"
+        }])
+        .then(function({name, role, id, email}) {
+            let roleInfo = "";
+            if (role === "Engineer") {
+                roleInfo = "GitHub username";
+            } else if (role === "Intern") {
+                roleInfo = "school name";
+            } else {
+                roleInfo = "office phone number";
+            }
+            inquirer.prompt([{
+                message: `Enter team member's ${roleInfo}`,
+                name: "roleInfo"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name: "moreMembers"
+            }])
+            .then(function({roleInfo, moreMembers}) {
+                let newMember;
+                if (role === "Engineer") {
+                    newMember = new Engineer(name, id, email, roleInfo);
+                } else if (role === "Intern") {
+                    newMember = new Intern(name, id, email, roleInfo);
+                } else {
+                    newMember = new Manager(name, id, email, roleInfo);
+                }
+                employees.push(newMember);
+                addHtml(newMember)
+                .then(function() {
+                    if (moreMembers === "yes") {
+                        newEmployee();
+                    }
+                });
+                
+            });
+        });
+}
+
+        function writeToFile() {
+            fs.writeFile('README-generated.md', markdown(), (err) => {
+                err ? console.error(err) : markdown; console.log('Your README.md has been generated');
+            });
+        }

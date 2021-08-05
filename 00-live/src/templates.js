80a0f7168c0d43894073b9c49a8c566c238b6f12
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-05 18:12:48 -0500

    added html templates to template folder

--- 00-live/src/templates.js
+++ 00-live/src/templates.js
@@ -1,3 +1,69 @@
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+const Manager = require("../lib/Manager");
+
+
+function createHTML(employees) {
+    let html = startHtml();
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].role === "Manager") {
+            html = html + managerC(employees[i]);
+        }
+        else if (employees[i].role === "Engineer") {
+            html = html + engineerC(employees[i]);
+        }
+        else { html = html + internC(employees[i]); }
+    }
+    html = html + finishHtml(); 
+    return html; 
+}
+
+function startHtml () {
+    const html = 
+    `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+        <link rel="stylesheet" href="./style.css">
+    <title>Team Profile Generator</title>
+</head>
+
+<body>
+    <section class="hero is-danger">
+        <div class="hero-body">
+            <p class="title has-text-centered">
+                My Team
+            </p>
+        </div>
+    </section>
+    <main class="section">
+        <div class="tile is-ancestor is-mobile is-vcentered is-justify-content-center">
+    `
+
+    return html;
+}
+
+function finishHtml() {
+    const html =
+        `
+        </div>
+    </main>
+</body>
+
+</html>
+`
+    return html;
+}
+
 
 
 function engineerC (){
@@ -96,3 +162,5 @@ return `<div class="card">
 </div>
 `;
 }
+
+module.exports 

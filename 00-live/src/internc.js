595dddf8e6438565791a8a1b7d51dfe3fc37e220
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-04 22:02:22 -0500

    making changes to markup cards

---
+++ 00-live/src/internc.js
@@ -0,0 +1,28 @@
+<div class="card">
+    <header class="media-content has-background-link">
+        <p class="title">
+            Name
+        </p>
+        <p class="subtitle">
+            Role
+        </p>
+    </header>
+    <div class="card-content">
+        <div class="content">
+            <table class="table is-bordered">
+                <tbody>
+                    <tr>
+                        <td>id:</td>
+                    </tr>
+                    <tr>
+                    <td>email:<a href = "mailto: abc@example.com">[email]</a></td>
+                    </tr>
+                    <tr>
+                        <td>school:</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
